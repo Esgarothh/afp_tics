4a70d274a3f55cafa3c5cfe1c84c2ded32de441a
Pablo Saez <pabloskys85@gmail.com>
2021-08-24 23:59:03 -0400

    pablo24-08

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'pablo/index'
   resources :datos
   get 'inicio/index'
-  root 'inicio#index'
+  get 'pablo/datos'
+
+  root 'pablo#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
