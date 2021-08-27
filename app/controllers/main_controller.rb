cbdc8c6dd0d0ee7e69ba765ae09585025dce62e5
Esgarothh <esgadiem@gmail.com>
2021-08-26 22:24:45 -0400

    ultimo

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,17 +1,21 @@
 class MainController < ApplicationController
 
-  def index
-    @Usuario = Usuario.all
+def index
     if params[:num] != nil
       @auxiliar = params[:num].to_i*2
     else
       @auxiliar = params[:num]
   end
+    
 end
 
-  def create
-    @auxiliar = params[:nombre]
-  end 
+def create
+    @usuario = Usuario.new(params:[nombre])
+    
+      redirect_to pablo_index_path
+  
+end
+  
 
 
 
